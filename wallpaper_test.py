a228294b9e1290ab582c27d7495bef8db52566f6
yesh233 <yesh2333@126.com>
2015-03-13 09:51:45 +0800

    add test

--- wallpaper_test.py
+++ wallpaper_test.py
@@ -7,14 +7,24 @@ from wallpaper import *
 
 class ImageDownloaderTestCase(unittest.TestCase):
     def setUp(self):
-        self.ImageDownloader = ImageDownloader(config.wallpaper_query_url, config.bing_base_url,
-                                               config.resolution_ratio, config.save_directory)
+        self.__image_downloader = ImageDownloader(config.wallpaper_query_url, config.bing_base_url,
+                                                  config.resolution_ratio, config.save_directory)
 
     def tearDown(self):
-        self.ImageDownloader = None
+        self.__image_downloader = None
 
     def test_get_image_save_path(self):
-        self.assertEqual(self.ImageDownloader.get_image_save_path(), 'abc')
+        self.assertEqual(self.__image_downloader.get_image_save_path(), '/home/yesh233/.bingWallpaper/' +
+                                                                        time.strftime('%y-%m-%d')+'.jpg')
+
+
+class WallpaperSetterTestCase(unittest.TestCase):
+    def setUp(self):
+        self.__wallpaper_setter = WallpaperSetter(config.set_wallpaper_command)
+
+    def tearDown(self):
+        self.__wallpaper_setter = None
+
 
 if __name__ == '__main__':
     unittest.main()
