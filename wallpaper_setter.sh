99363b87f61c9498bec9b8ec79870f40bd08493e
yesh233 <yesh2333@126.com>
2015-03-20 20:28:17 +0800

    change pid list 2 pid

--- wallpaper_setter.sh
+++ wallpaper_setter.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 PID=$(pgrep gnome-session)
-eval PID=($PID)
+#eval PID=($PID)
 
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/${PID[1]}/environ|cut -d= -f2-)
+#export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/${PID[1]}/environ|cut -d= -f2-)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
 
 gsettings set org.gnome.desktop.background picture-uri "$1"
